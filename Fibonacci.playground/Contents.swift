969d9513f74919c73b57a77cf3e4167a62f42d68
guillaume verpoest <guillaume.verpoest@gmail.com>
2020-08-02 16:41:06 +0200

    Update

--- Fibonacci.playground/Contents.swift
+++ Fibonacci.playground/Contents.swift
@@ -1,46 +1,40 @@
-internal class Fibonacci {
+class Fibonacci {
     var n:Int
-    // MARK: - INITIALISATION
-    fileprivate init(n:Int) {
+    init(n:Int){
         self.n = n
     }
     
-    // MARK: -RECURSIVE
-     fileprivate func recursive(n:Int = instance.n) -> Int
-    {
-        if n == 0
-        {
+    // MARK: - RECURSIVE
+    func recursive(n:Int) -> Int{
+        if n == 0 {
             return 0
-        }
-        else if n == 1
-        {
+        }else if n == 1{
+            n == 1
             return 1
+        }else{
+            return recursive(n: n-1) + recursive(n: n-2)
         }
-        else
-        {
-            return recursive(n:n-1) + recursive(n:n-2)
-        }
-
     }
-    // MARK: - BOTTOM_TOP
-    fileprivate func bottom_top(n:Int = instance.n) -> Int
-    {
+    
+    
+    // MARK: - BOTTOMTOP
+    func bottomTop() -> Int{
         var array = [0,1]
-        for index in Range(2...n) {
+        for index in Range(2...self.n){
             array.append(0)
             array[index] = array[index-1] + array[index-2]
         }
-        return array[n]
+        return array[self.n]
     }
+    
+    
     // MARK: - ITERATION
-    fileprivate func iteration(i:Int = instance.n) ->Int
-    {
-        var cpt: Int = n-1
+    func iteration() -> Int{
+        var cpt: Int = self.n-1
         var first: Int = 0
         var seconde: Int = 1
-        var tmp:Int
-        while cpt >= 0
-        {
+        var tmp: Int
+        while cpt >= 0 {
             tmp = first+seconde
             first = seconde
             seconde = tmp
@@ -50,16 +44,9 @@ internal class Fibonacci {
     }
 }
 
-var instance  = Fibonacci(n:16)
-print("Recursive -> ",instance.recursive())
-print("Bottom_top -> ",instance.bottom_top())
-print("iteration -> ", instance.iteration())
+let instance = Fibonacci(n:16)
 
+print("Recursive -> " ,instance.recursive(n: instance.n))
+print("Bottom_top -> " ,instance.bottomTop())
+print("iteration -> " ,instance.iteration())
 
-
-
-//array 2 dimmension
-//horizontal - vertical
-//3 index
-// 1->3
-// combien de 1 sont côte a côte 
